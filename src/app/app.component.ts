2e589305c751d8dc903a30db8361decc94d7711f
adcao0128 <adc135@case.edu>
2024-05-20 07:01:53 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,127 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './models/employee';
+import { EmployeeService} from './services/employee.service'
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, FormsModule],
+  providers: [HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'employeemanagerapp';
+
+  public employees: Employee[] = [];
+  public editEmployee?: Employee;
+  public deleteEmployee? : Employee;
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (response: Employee[]) => {
+        this.employees = response;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+  });
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+  })
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe({
+      next: (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+  })
+  }
+
+  public onDeleteEmployee(employeeId?: number): void {
+    if (employeeId !== undefined) {
+      this.employeeService.deleteEmployee(employeeId).subscribe({
+        next: (response: void) => {
+          console.log(response);
+          this.getEmployees();
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      })
+    }
+  }
+
+  public searchEmployees(key: string): void {
+    const results: Employee[] = [];
+    if (key && key.trim() !== ''){
+      for (const employee of this.employees) {
+        if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+          results.push(employee);
+        }
+      }
+      this.employees = results;
+      if (results.length === 0 || !key) {
+        this.getEmployees();
+      }
+    } else {
+      this.getEmployees();
+    }
+
+    
+  }
+
+
+  public onOpenModal(mode: string, employee?: Employee): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit'){
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete'){
+      this.deleteEmployee = employee
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    if (container) {
+      container.appendChild(button);
+      button.click();
+    }
+
+  }
 }
